dc51534274869653b8e130faa8ff2a506014bd81
nikolamiljkovic91 <nikolamiljkovic91@gmail.com>
2020-02-27 15:14:58 +0100

    Updates

---
+++ precache-manifest.43788020787c5a8e31459d306a1e49bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6695af3c6c51eadff23447c438294a07",
+    "url": "/cryptocurrency/index.html"
+  },
+  {
+    "revision": "fdbe2c4059e7de25de64",
+    "url": "/cryptocurrency/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "de4e80db428495607c87",
+    "url": "/cryptocurrency/static/css/main.02829776.chunk.css"
+  },
+  {
+    "revision": "fdbe2c4059e7de25de64",
+    "url": "/cryptocurrency/static/js/2.0664ed39.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/cryptocurrency/static/js/2.0664ed39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de4e80db428495607c87",
+    "url": "/cryptocurrency/static/js/main.72eaf966.chunk.js"
+  },
+  {
+    "revision": "f504ff19eaa8d52fc509",
+    "url": "/cryptocurrency/static/js/runtime-main.17d624b4.js"
+  },
+  {
+    "revision": "83b41f772dbf1b0f3501141e99dc2f66",
+    "url": "/cryptocurrency/static/media/IMG1.83b41f77.JPG"
+  }
+]);
